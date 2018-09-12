6eed9fc90c4bd0fb4ce2151c1aed60beb66f6653
Esteban <krisstoferos@gmail.com>
2018-09-12 13:40:25 +0200

    Initial Commit

---
+++ AnimatedStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedStyle
+//
+//  Created by Esteban on 12.09.2018.
+//  Copyright © 2018 Selfcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
